1f782618922d49a43b4bb16f87f08a91158901bf
Stas Cartoon <bycs@mail.ru>
2022-07-15 06:11:29 +0300

    Added the ability to get a list of bars.

--- beers/serializers.py
+++ beers/serializers.py
@@ -1,3 +1,4 @@
+from beers.models.bars import Bar, BarBranch
 from beers.models.beers import Beer
 
 from rest_framework import serializers
@@ -9,4 +10,20 @@ class BeerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Beer
         fields = ("name", "price", "description", "specifications", "bar")
+        http_method_names = ("get",)
+
+
+class BarSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Bar
+        fields = ("name", "website")
+        http_method_names = ("get",)
+
+
+class BarBranchSerializer(serializers.HyperlinkedModelSerializer):
+    bar = serializers.CharField(source="bar.name", read_only=True)
+
+    class Meta:
+        model = BarBranch
+        fields = ("bar", "metro", "address")
         http_method_names = ("get",)
--- server/urls.py
+++ server/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/beers/", include("beers.urls")),
+    path("api/v1/", include("beers.urls")),
 ]
