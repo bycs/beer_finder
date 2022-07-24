2f48e005ccf2555bd6ffc75760d1c21bccc5a1ba
Stas Cartoon <bycs@mail.ru>
2022-07-24 16:53:00 +0300

    add path /api/v1/top_spec_beers/

--- beers/views.py
+++ beers/views.py
@@ -7,6 +7,8 @@ from beers.serializers import BarBranchSerializer, BarSerializer, BeerSerializer
 from django.http import HttpResponse
 
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 @staff_required
@@ -17,16 +19,38 @@ def sync_all_beers(request):
     return HttpResponse("Все ок!")
 
 
+@api_view(["GET"])
+def top_spec_beers(request):
+    queryset = Beer.beer_managers.all().values("specifications")
+    specifications = [item["specifications"] for item in queryset]
+    keys = set()
+    keys_list = []
+    key_count = {}
+
+    for spec in specifications:
+        keys.update(spec.keys())
+        keys_list += list(spec.keys())
+    for key in keys:
+        key_count[key] = keys_list.count(key)
+
+    key_count = dict(sorted(key_count.items(), key=lambda item: item[1], reverse=True))
+    top_keys = list(key_count.keys())[:7]
+    return Response(data=top_keys)
+
+
 class BeersViewSet(viewsets.ModelViewSet):
     queryset = Beer.beer_managers.all().order_by("name")
     serializer_class = BeerSerializer
+    http_method_names = ("get",)
 
 
 class BarsViewSet(viewsets.ModelViewSet):
     queryset = Bar.objects.all().order_by("name")
     serializer_class = BarSerializer
+    http_method_names = ("get",)
 
 
 class BarBranchesViewSet(viewsets.ModelViewSet):
     queryset = BarBranch.objects.all().order_by("bar__name")
     serializer_class = BarBranchSerializer
+    http_method_names = ("get",)
