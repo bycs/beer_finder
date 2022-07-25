4060486e96e85d7c50b5fe70bc1a3bfc2aeab240
Stanis96 <vasilievstas96@yandex.ru>
2022-07-25 15:54:15 +0300

    Added parsers/we_all_cidreria

---
+++ beers/parsers/we_all_cidreria.py
@@ -0,0 +1,55 @@
+import re
+
+from beers.parsers.base import get_html
+
+from bs4 import BeautifulSoup
+
+base_url = "https://we-cidreria.ru"
+beer_url = ["/menu/beer/"]
+cidr_url = ["/menu/ciders/"]
+
+
+def get_assortment_we_cidreria(html: str) -> list:
+    soup = BeautifulSoup(html, "html.parser")
+    all_beers = soup.find_all("div", class_="catalog-element-item")
+    beer_list = []
+    for beer in all_beers:
+        name = beer.find("div", class_="catalog-element-name").text
+        description = beer.find("p", class_="catalog-element-ru").text
+        strength = getattr(beer.find("div", class_="catalog-element-weight"), "text", None)
+        size = beer.find("div", class_="catalog-element-price-item").text
+        cost = beer.find("div", class_="catalog-element-price-value").text
+        size = re.findall(r"\d+,\d+", size)
+        cost = re.findall(r"\d+", cost)
+        price_list = []
+        for i in range(len(size)):
+            cost_item = int(cost[i])
+            size_item = float(size[i].replace(",", "."))
+            price_item = int(100 * cost_item / size_item)
+            price_list.append(price_item)
+
+            if price_list:
+                price = min(price_list)
+            else:
+                price = None
+            specifications_dict = {"Крепость": strength}
+            beer_list.append(
+                {
+                    "name": name,
+                    "price": price,
+                    "description": description,
+                    "specifications": specifications_dict,
+                }
+            )
+            new_value = {i["name"]: i for i in beer_list}
+            beer_list = list(new_value.values())
+    return beer_list
+
+
+def get_all_we_cidreria(base_url: str, cidr_url: list, beer_url: list):
+    all_list = []
+    html = get_html(f"{base_url}{beer_url[0]}")
+    html2 = get_html(f"{base_url}{cidr_url[0]}")
+    all_list += get_assortment_we_cidreria(html)
+    all_list += get_assortment_we_cidreria(html2)
+    return all_list
