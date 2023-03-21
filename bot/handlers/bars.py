22244c4c3a5f3fc483d110f16e8fb2e4d5618f74
Stas Cartoon <bycs@mail.ru>
2023-03-21 14:51:47 +0300

    Updated function command_bars

--- bot/handlers/bars.py
+++ bot/handlers/bars.py
@@ -1,8 +1,9 @@
+from django.db.models.query import QuerySet
 from telegram import Update
 from telegram.ext import CommandHandler
 from telegram.ext import ContextTypes
 
-from beers.logics.utils import get_bars
+from beers.models.bars import Bar
 from bot.db import db
 from bot.db import logging_commands
 
@@ -10,10 +11,10 @@ from bot.db import logging_commands
 async def command_bars(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     assert update.message is not None, "update.message must not be None"
 
-    bars = list(get_bars())
+    bars: QuerySet = Bar.objects.order_by("name").values("name", "website").distinct()
     bars_text = ""
     for bar in bars:
-        bars_text += f"🍻 {bar.name}\nСайт: {bar.website}\n\n"
+        bars_text += f"🍻 {bar['name']}\nСайт: {bar['website']}\n\n"
     text = "Сейчас мы знаем о следующих барах:\n\n" + bars_text
     await update.message.reply_text(text, disable_web_page_preview=True)
     logging_commands(db, update, "bars")
